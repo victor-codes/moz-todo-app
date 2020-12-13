41504ca6c2dc8c2875ef5d0d3e7009b4cbfb8fb4
AYODEJI <ayodejiv5@gmail.com>
2020-12-13 02:02:41 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+import { Todo } from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import usePrevious from "./components/UsePrevious";
 
-function App() {
+const FILTER_MAP = {
+  All: () => true,
+  active: (task) => !task.completed,
+  completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
+  function addTask(name) {
+    const newTasks = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTasks]);
+  }
+
+  function editTask(id, newName) {
+    const editedTask = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...tasks, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTask);
+  }
+
+  function toggleTaskCompleted(id) {
+    const updateTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+
+    setTasks(updateTasks);
+    console.log(tasks[0]);
+  }
+
+  function deleteTask(id) {
+    const remainingTask = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTask);
+    console.log(id);
+  }
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
+  const taskNoun = taskList.length > 1 ? "tasks" : "task";
+  const taskHeader = `${taskList.length} ${taskNoun} remaining`;
+
+  const listHeadingRef = useRef(null);
+  const prevTaskLength = usePrevious(tasks.length);
+
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">{filterList}</div>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+        {taskHeader}
+      </h2>
+
+      <ul
+        role="list"
+        className="todo-list stack-large stack-execption"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App tasks={DATA} />,
+  document.getElementById("root")
+  // console.log(props.tasks)
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
